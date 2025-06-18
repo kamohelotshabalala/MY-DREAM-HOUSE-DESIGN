20580df842944b17b9452a00e0b8b8a16d4a5fa5
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-18 15:22:42 +0200

    Added description element

--- src/index.js
+++ src/index.js
@@ -1,12 +1,20 @@
 function clickButton(event) {
     event.preventDefault();
     alert("heyy");
+    let descriptionElement = document.querySelector("#description");
+    descriptionElement.innerHTML =`  
+    1.Recent renovations and upgrades.
+   2. Notable brands of appliances, flooring, lighting, etc.
+   3. Lot size.
+    4.Basement/garage.
+   5. Additional features: pool, fireplace, deck, secret coding room etc.
+    Unique characteristics: historic property, ocean views, etc.`
 
     new Typewriter("#description", {
-      strings: ["Hello", "World"],
+      strings: "description",
       autoStart: true,
       cursor: null,
     });
 }
 let buttonElement = document.querySelector("button");
-buttonElement.addEventListener("click", clickButton);
+buttonElement.addEventListener("click", clickButton)
