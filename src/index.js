5baf29dba3e365d50e02932cfa6e2c125eb29f1c
kamohelotshabalala <kamohelotshabalala32@gmail.com>
2025-06-21 13:43:50 +0200

    Changed paragraph into a div

--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ function clickButton(event) {
     event.preventDefault();
     alert("heyy");
     let descriptionElement = document.querySelector("#description");
-    descriptionElement.innerHTML =`  
+     descriptionHTML += ` <div class= "description">
     1.Recent renovations and upgrades.
    2. Notable brands of appliances, flooring, lighting, etc.
    3. Lot size.
     4.Basement/garage.
    5. Additional features: pool, fireplace, deck, secret coding room etc.
-    Unique characteristics: historic property, ocean views, etc.`
+    Unique characteristics: historic property, ocean views, etc. </div>`;
+ descriptionElement.innerHTML = descriptionHTML;
+   
 
     new Typewriter("#description", {
-      strings: "description",
+      strings: "",
       autoStart: true,
       cursor: null,
     });
